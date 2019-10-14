49f891cb00ed6c81bb1d56e22c76d6c3eb933489
maudnals <maud.nalpas@gmail.com>
2019-10-14 12:23:22 +0200

    feat: added intro step

---
+++ Introduction.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import {
+  bodyTxt,
+  mainTitleTxt,
+  mainViewContainer,
+  sectionContainer,
+  sectionTitleTxt,
+} from './sharedStyles'
+import { Button } from 'react-native'
+
+class Introduction extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
+
+  render(): React.ReactNode {
+    const { navigate } = this.props.navigation
+    return (
+      <View style={mainViewContainer}>
+        <View style={sectionContainer}>
+          <Text style={mainTitleTxt}>Claim independence.</Text>
+        </View>
+        <View style={sectionContainer}>
+          <Text style={sectionTitleTxt}>
+            This is your very own KILT wallet.
+          </Text>
+          <Text style={bodyTxt}>Here, you can... (description)</Text>
+        </View>
+        <Button
+          title="Get started >"
+          onPress={() => navigate('IdentityCreation')}
+        />
+      </View>
+    )
+  }
+}
+
+export default Introduction
