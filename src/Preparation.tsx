9bf816671156339630b84135e187e3f28b1ad8b9
maudnals <maud.nalpas@gmail.com>
2019-10-14 18:02:34 +0200

    feat: displayed prep steps progress

--- src/Preparation.tsx
+++ src/Preparation.tsx
@@ -1,27 +1,64 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import * as Kilt from '@kiltprotocol/sdk-js'
-import {
-  mainViewContainer,
-  sectionContainer,
-  sectionTitleTxt,
-} from './styles/sharedStyles'
+import { mainViewContainer, sectionContainer } from './styles/utils.layout'
+import { sectionTitleTxt } from './styles/utils.typography'
 import { Button } from 'react-native'
 import PreparationStep from './PreparationStep'
+import { StepStatus } from './enums'
 
-class Preparation extends React.Component {
+type Props = {
+  navigation: any
+}
+type State = {
+  steps: any
+}
+
+class Preparation extends React.Component<Props, State> {
   static navigationOptions = {
     header: null,
   }
 
-  // todo forbid back navigation
-
-  createIdentity(mnemonic) {
-    const identity = Kilt.Identity.buildFromMnemonic(mnemonic)
+  state = {
+    steps: {
+      create: {
+        description: 'Creating your identity',
+        status: StepStatus.Pending,
+      },
+      save: {
+        description: 'Saving your identity',
+        status: StepStatus.NotStarted,
+      },
+    },
   }
 
-  async componentDidMount() {
-    // console.log('hi')
+  createIdentity = (mnemonic: string) =>
+    Kilt.Identity.buildFromMnemonic(mnemonic)
+
+  createIdentityAsync = (mnemonic: string) =>
+    new Promise(resolve =>
+      setTimeout(() => resolve(this.createIdentity(mnemonic)), 2000)
+    )
+
+  async componentDidMount(): Promise<void> {
+    const mnemonic = this.props.navigation.getParam('mnemonic')
+    const identity = await this.createIdentityAsync(mnemonic)
+    if (identity) {
+      this.setState(prevState => ({
+        steps: {
+          create: {
+            ...prevState.steps.create,
+            status: StepStatus.Success,
+          },
+          save: {
+            ...prevState.steps.save,
+            status: StepStatus.Pending,
+          },
+        },
+      }))
+    }
+    // console.log(identity)
+
     // const blockchain = await Kilt.default.connect(
     //   'wss://full-nodes.kilt.io:9944'
     // )
@@ -29,6 +66,8 @@ class Preparation extends React.Component {
 
   render(): React.ReactNode {
     const { navigate } = this.props.navigation
+    const { steps } = this.state
+    // const mnemonic = this.props.navigation.getParam('mnemonic')
     return (
       <View style={mainViewContainer}>
         <View style={sectionContainer}>
@@ -36,16 +75,16 @@ class Preparation extends React.Component {
             Step 2: Knitting your KILT account together
           </Text>
         </View>
-        <View style={sectionContainer}>
-          <PreparationStep stepName="Creating your identity" />
-        </View>
-        <View style={sectionContainer}>
-          <PreparationStep stepName="Saving your identity" />
-        </View>
-        <View style={sectionContainer}>
-          <PreparationStep stepName="Tranferring initial tokens to you" />
-        </View>
-        <Button title="Next >" onPress={() => navigate('')} />
+        {Object.values(steps).map(step => (
+          <View style={sectionContainer}>
+            <PreparationStep
+              description={step.description}
+              status={step.status}
+            />
+          </View>
+        ))}
+        {/* enabled only if all steps were successful */}
+        <Button title="Next >" onPress={() => navigate('Home')} />
       </View>
     )
   }
