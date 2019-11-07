a0b967b591c5059c41c3a45198e0f6248107e80b
maudnals <maud.nalpas@gmail.com>
2019-11-07 10:00:42 +0100

    feat: added dummy attester

--- src/_config.ts
+++ src/_config.ts
@@ -1,3 +1,5 @@
 export const BLOCKCHAIN_NODE = 'wss://full-nodes.kilt.io:9944'
-
 export const MESSAGING_SERVICE_URL = 'https://services.kilt.io:443/messaging'
+export const ATTESTER_MNEMONIC =
+  'snake rabbit relief hotel naive quiz chicken square office identify obscure tired'
+// 5FVqnXPPCTBKtkJMvBnifLiXFfXhNQnxh7xSmmYmXx7J1zux
