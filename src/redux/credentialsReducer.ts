9138c48eb3cd813579071993ec0449af8ec4fee6
maudnals <maud.nalpas@gmail.com>
2019-11-07 16:12:07 +0100

    refactor: simplified cred update code

--- src/redux/credentialsReducer.ts
+++ src/redux/credentialsReducer.ts
@@ -31,23 +31,21 @@ export default function credentialsReducer(
       }
     case UPDATE_CREDENTIAL_STATUS:
       console.log('updating credential status', action.payload.hash)
-      // TODO fix
-      if (state.credentialsAsObject[action.payload.hash]) {
-        const newState = {
-          ...state,
-          credentialsAsObject: {
-            ...state.credentialsAsObject,
-            [action.payload.hash]: {
-              ...state.credentialsAsObject[action.payload.hash],
-              status: action.payload.status,
+      const { hash: claimHash, status: claimStatus } = action.payload
+      const credentialToUpdate = state.credentialsAsObject[claimHash]
+      return {
+        ...state,
+        credentialsAsObject: {
+          ...state.credentialsAsObject,
+          // only update the credential if a relevant update msg is sent
+          ...(credentialToUpdate && {
+            [claimHash]: {
+              ...credentialToUpdate,
+              status: claimStatus,
             },
-          },
-        }
-        console.log('========))))))', newState.credentialsAsObject)
-        return newState
+          }),
+        },
       }
-      return state
-
     default:
       return state
   }
