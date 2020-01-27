e95bde698eb34f3024c9a1646ddb32ee3a3987ef
maudnals <maud.nalpas@gmail.com>
2020-01-27 16:20:12 +0100

    feat: prepared routes for new claim

--- src/_config.ts
+++ src/_config.ts
@@ -1,5 +1,3 @@
-const ATTESTER_MNEMONIC =
-  'daring able century salute oxygen purse hub boat dry three core opera'
 const BLOCKCHAIN_NODE = 'wss://full-nodes.kilt.io:9944'
 const MESSAGING_SERVICE_URL = 'https://services.kilt.io:443/messaging'
 const CONTACTS_SERVICE_URL = 'https://services.kilt.io:443/contacts'
@@ -7,7 +5,6 @@ const MESSAGING_INBOX_URL = `${MESSAGING_SERVICE_URL}/inbox`
 const POLLING_PERIOD_MS = 3000
 
 export {
-  ATTESTER_MNEMONIC,
   BLOCKCHAIN_NODE,
   MESSAGING_INBOX_URL,
   MESSAGING_SERVICE_URL,
