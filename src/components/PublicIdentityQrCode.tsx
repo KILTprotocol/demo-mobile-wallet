83938c006cebf24411ee5880b3be60df06b9ffb8
maudnals <maud.nalpas@gmail.com>
2020-01-27 16:17:09 +0100

    refactor: switched QR Codes from addresses to public ids

--- src/components/PublicIdentityQrCode.tsx
+++ src/components/PublicIdentityQrCode.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import { View } from 'react-native'
 import { QRCode } from 'react-native-custom-qr-codes'
+import { PublicIdentity } from '@kiltprotocol/sdk-js'
 import { CLR_KILT_1_DARK } from '../sharedStyles/styles.consts.colors'
 import { flexRowCenter } from '../sharedStyles/styles.layout'
-import { IPublicIdentity } from '@kiltprotocol/sdk-js'
 const kiltLogoSquare = require('../assets/imgs/kiltLogoSquare.jpg')
 
 type Props = {
-  address: IPublicIdentity['address']
+  publicIdentity: PublicIdentity | null
 }
 
-const AddressQrCode: React.FunctionComponent<Props> = ({
-  address,
+const PublicIdentityQrCode: React.FunctionComponent<Props> = ({
+  publicIdentity,
 }): JSX.Element => (
   <View style={flexRowCenter}>
     <QRCode
       codeStyle="circle"
-      content={address}
+      content={JSON.stringify(publicIdentity || '')}
       color={CLR_KILT_1_DARK}
-      size={190}
+      size={220}
       logo={kiltLogoSquare}
       logoSize={50}
     />
   </View>
 )
 
-export default AddressQrCode
+export default PublicIdentityQrCode
