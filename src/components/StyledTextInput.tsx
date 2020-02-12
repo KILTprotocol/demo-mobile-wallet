2070267976c3f77fc7614bfcbd0cb64b6626d098
maudnals <maud.nalpas@gmail.com>
2020-02-12 17:01:26 +0100

    feat: created reusable styled text input

--- src/components/StyledTextInput.tsx
+++ src/components/StyledTextInput.tsx
@@ -4,15 +4,15 @@ import { CONFIG_THEME } from '../config'
 import { bodyTxt, bodyInvertedClrTxt } from '../sharedStyles/styles.typography'
 import { input } from '../sharedStyles/styles.form'
 
-type Props = TextInputProps & { inverted?: boolean }
+type Props = TextInputProps & { inverted?: boolean; extraStyle?: object }
 
 const StyledTextInput: React.FunctionComponent<Props> = (
   props
 ): JSX.Element => {
-  const { inverted } = props
+  const { inverted, extraStyle } = props
   const style = inverted
-    ? [bodyTxt, input, bodyInvertedClrTxt]
-    : [bodyTxt, input]
+    ? [bodyTxt, input, extraStyle, bodyInvertedClrTxt]
+    : [bodyTxt, input, extraStyle]
   return (
     <TextInput
       {...props}
