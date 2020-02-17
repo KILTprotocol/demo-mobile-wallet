82a1ab6870a8d9ad0d8fbcd6bdfc066dfaed9d70
maudnals <maud.nalpas@gmail.com>
2020-02-17 11:50:56 +0100

    feat: simplified color generation

--- src/utils/utils.formatting.ts
+++ src/utils/utils.formatting.ts
@@ -16,8 +16,7 @@ const ADDRESS_DISPLAY_END_LENGTH = 4
 const truncateAddress = (
   address: IPublicIdentity['address'],
   startLength = ADDRESS_DISPLAY_START_LENGTH
-): string =>
-  `${address.substr(0, startLength)}...${address.substr(
+): string => `${address.substr(0, startLength)}...${address.substr(
     address.length - ADDRESS_DISPLAY_END_LENGTH,
     ADDRESS_DISPLAY_END_LENGTH
   )}`
@@ -27,13 +26,8 @@ but always the same for a given string
 useful e.g. to generate a color for an address, that will be the same for 2
 different users adding the same contact (better for UX), but likely different for 2 different addresses */
 const generateConstantColorFromStr = (str: string): string => {
-  // sum all character codes of the str
-  const sum = str.split('').reduce((acc, c, idx) => {
-    return acc + str.charCodeAt(idx)
-  }, 0)
-  // last digit of the sum; last is arbitrary, might induces more variablity
-  const lastDigit = parseInt(`${sum}`[`${sum}`.length - 1], 10)
-  return COLORS[lastDigit % COLORS.length]
+  const lastCharCode = str.charCodeAt(str.length - 1)
+  return COLORS[lastCharCode % COLORS.length]
 }
 
 /* ---------------------------------- */
