42a30c2d1522075399fae780c4a067fe98872600
maudnals <maud.nalpas@gmail.com>
2020-03-03 15:24:25 +0100

    style: eslint fixes

--- src/components/ClaimList.tsx
+++ src/components/ClaimList.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { View } from 'react-native'
+import {
+  NavigationScreenProp,
+  NavigationState,
+  NavigationParams,
+} from 'react-navigation'
 import { sectionContainer } from '../sharedStyles/styles.layout'
 import ClaimCard from './ClaimCard'
 import { TClaim } from '../types'
 
 type Props = {
   claims: TClaim[]
+  navigation: NavigationScreenProp<NavigationState, NavigationParams>
 }
 
-const ClaimList: React.FunctionComponent<Props> = (props): JSX.Element => {
-  const { claims } = props
+const ClaimList: React.FunctionComponent<Props> = ({
+  claims,
+  navigation,
+}): JSX.Element => {
   // sort claims by creation date, newest on top
   const sortedClaims = [
-    ...claims.sort((claimA, claimB) => claimA.requestTimestamp > claimB.requestTimestamp ? -1 : 1),
+    ...claims.sort((claimA, claimB) =>
+      claimA.requestTimestamp > claimB.requestTimestamp ? -1 : 1
+    ),
   ]
   return (
     <>
@@ -22,6 +32,8 @@ const ClaimList: React.FunctionComponent<Props> = (props): JSX.Element => {
             title={claim.title}
             status={claim.status}
             contents={claim.contents}
+            navigation={navigation}
+            claimHash={claim.hash}
           />
         </View>
       ))}
